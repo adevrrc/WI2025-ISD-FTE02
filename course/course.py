7c3927f9d4e014832f1d0628397fd46b428ba6b9
Damien Altenburg <daltenburg@rrc.ca>
2025-01-08 16:03:24 -0600

    Update to Course class. Finished implementing the __init__ method and added all the other methods.

--- course/course.py
+++ course/course.py
@@ -16,8 +16,46 @@ class Course:
             department (Department): The department the course is 
                 delivered.
             credit_hours (int): The number of credit hours.
+
+        Raises:
+            TODO
         """
+
+        name = name.strip()
+        
+        if len(name) == 0:
+            raise ValueError("Name cannot be blank.")
+        
+        # TODO: need to speak to Damien about this.
+        # if department not in Department:
+        #     raise ValueError("Department is not valid.")
+
+        if not isinstance(credit_hours, int):
+            raise ValueError("Credit Hours must be numeric.")
 
         self.__name = name
         self.__department = department
         self.__credit_hours = credit_hours
+
+    @property
+    def name(self) -> str:
+        """TODO"""
+
+        return self.__name
+    
+    @property
+    def department(self) -> Department:
+        return self.__department
+    
+    @property
+    def credit_hours(self) -> int:
+        return self.__credit_hours
+    
+    def __str__(self) -> str:
+        """TODO"""
+
+        string_representation = f"Course: {self.name}\n" \
+            f"Department: {self.department.name.replace("_", " ").title()}\n" \
+            f"Credit Hours: {self.credit_hours}"
+
+        return string_representation
