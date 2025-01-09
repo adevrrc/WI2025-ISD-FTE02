d0c3686934c79afd3887ebf87fcf0719d1c05670
Damien Altenburg <daltenburg@rrc.ca>
2025-01-09 15:50:23 -0600

    Update to complete the definition of the TestCourse class. Includes all unit test functions.

--- tests/test_course.py
+++ tests/test_course.py
@@ -12,21 +12,100 @@ from department.department import Department
 from course.course import Course
 
 class TestCourse(unittest.TestCase):
-    """TODO"""
+    """Tests for the Course class."""
+     
+    def setUp(self):
+        # Setup runs AUTOMATICALLY before each test method and
+        # provides initial values for the class attributes.
+        course_name = "intermediate software development"
+        department = Department.COMPUTER_SCIENCE
+        credit_hours = 90
+
+        self.course = Course(course_name, department, credit_hours)
 
     def test_init_object_initialized_to_correct_state(self):
+        # Assert
+        self.assertEqual("intermediate software development", self.course._Course__name)
+        self.assertEqual(Department.COMPUTER_SCIENCE, self.course._Course__department)
+        self.assertEqual(90, self.course._Course__credit_hours)
+
+    def test_init_blank_name_raises_exception(self):
         # Arrange
-        name = "intermediate software development"
+        course_name = "    "
         department = Department.COMPUTER_SCIENCE
         credit_hours = 90
 
         # Act
-        course = Course(name, department, credit_hours)
+        with self.assertRaises(ValueError) as context:
+            course = Course(course_name, department, credit_hours)
 
         # Assert
-        self.assertEqual("intermediate software development", course._Course__name)
-        self.assertEqual(Department.COMPUTER_SCIENCE, course._Course__department)
-        self.assertEqual(90, course._Course__credit_hours)
+        expected = "Name cannot be blank."
+        actual = str(context.exception)
+        self.assertEqual(expected, actual)
+
+    def test_init_invalid_department_raises_exception(self):
+        # Arrange
+        course_name ="intermediate software development"
+        department = "computer science"
+        credit_hours = 90
+
+        # Act
+        with self.assertRaises(ValueError) as context:
+            course = Course(course_name, department, credit_hours)
+
+        # Assert
+        expected = "Department is not valid."
+        actual = str(context.exception)
+        self.assertEqual(expected, actual)
+
+    def test_init_invalid_credit_hours_raises_exception(self):
+        # Arrange
+        course_name ="intermediate software development"
+        department = Department.COMPUTER_SCIENCE
+        credit_hours = 34.5
+
+        with self.assertRaises(ValueError) as context:
+            course = Course(course_name, department, credit_hours)
+
+        # Assert
+        expected = "Credit Hours must be numeric."
+        actual = str(context.exception)
+        self.assertEqual(expected, actual)
+
+    def test_name_accessor_returns_correct_state(self):
+        # Act
+        actual = self.course.name
+
+        # Assert
+        expected = "intermediate software development"
+        self.assertEqual(expected, actual)
+
+    def test_department_accessor_returns_correct_state(self):
+        # Act
+        actual = self.course.department
+
+        # Assert
+        expected = Department.COMPUTER_SCIENCE
+        self.assertEqual(expected, actual)
+
+    def test_credit_hours_accessor_returns_correct_state(self):
+        # Act
+        actual = self.course.credit_hours
+
+        # Assert
+        expected = 90
+        self.assertEqual(expected, actual)
+
+    def test_str_returns_string_representation(self):
+        # Act
+        actual = self.course.__str__()        
+
+        # Assert
+        expected = ("Course: Intermediate Software Development\n"
+                    "Department: Computer Science\n"
+                    "Credit Hours: 90")
+        self.assertEqual(expected, actual)
 
 if __name__ == "__main__":
     unittest.main()
